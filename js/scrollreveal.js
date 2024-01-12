eb9f4a34874f0749e6d6fe65f6754c7dc3450c86
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2024-01-12 21:54:05 +0200

    add Scroll reveal

---
+++ js/scrollreveal.js
@@ -0,0 +1,26 @@
+ScrollReveal().reveal("header", { delay: 300, distance: "-10px", duration: 1000 });
+ScrollReveal().reveal(".hero-section__title", { delay: 800, distance: "-20px", duration: 1500 });
+ScrollReveal().reveal(".hero-section__text", { delay: 1000, distance: "-30px", duration: 1500 });
+ScrollReveal().reveal(".hero-section__btn", { delay: 1600, distance: "-50px", duration: 1500 });
+ScrollReveal().reveal(".section-title", { delay: 300, distance: "-20px", duration: 1000 });
+ScrollReveal().reveal(".advantages-section__card", {
+  delay: 500,
+  distance: "-50px",
+  duration: 2000,
+});
+ScrollReveal().reveal(".review-section__item", {
+  delay: 500,
+  distance: "-50px",
+  duration: 2000,
+});
+ScrollReveal().reveal(".footer__nav", {
+  delay: 300,
+  distance: "-50px",
+  duration: 2000,
+});
+ScrollReveal().reveal(".footer__nav-box", {
+  delay: 500,
+  distance: "-50px",
+  duration: 2000,
+});
+
