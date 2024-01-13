5b93f60afb1293e3098c68c31649248483686713
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2024-01-13 15:40:53 +0200

    Add pricing section, some of payment section

--- js/scrollreveal.js
+++ js/scrollreveal.js
@@ -2,13 +2,19 @@ ScrollReveal().reveal("header", { delay: 300, distance: "-10px", duration: 1000 
 ScrollReveal().reveal(".hero-section__title", { delay: 800, distance: "-20px", duration: 1500 });
 ScrollReveal().reveal(".hero-section__text", { delay: 1000, distance: "-30px", duration: 1500 });
 ScrollReveal().reveal(".hero-section__btn", { delay: 1600, distance: "-50px", duration: 1500 });
-ScrollReveal().reveal(".section-title", { delay: 300, distance: "-20px", duration: 1000 });
+ScrollReveal().reveal(".section-title", { delay: 400, distance: "-20px", duration: 1000 });
+ScrollReveal().reveal(".section-subtitle", { delay: 200, distance: "-20px", duration: 1000 });
 ScrollReveal().reveal(".advantages-section__card", {
   delay: 500,
   distance: "-50px",
   duration: 2000,
 });
 ScrollReveal().reveal(".review-section__item", {
+  delay: 500,
+  distance: "-50px",
+  duration: 2000,
+});
+ScrollReveal().reveal(".pricing-section__item", {
   delay: 500,
   distance: "-50px",
   duration: 2000,
