b94c11d164ef206276da238c985c5f48a7c24cb1
Alexander Rostyagai <136843641+Rostyagai@users.noreply.github.com>
2024-02-01 15:31:05 +0200

    Add pricing functionality

--- js/index.js
+++ js/index.js
@@ -2,7 +2,6 @@
 const openMenuBtn = document.getElementById("open-menu-btn");
 const closeMenuBtn = document.getElementById("close-menu-btn");
 const menu = document.getElementById("header-menu");
-// const menuLink = document.querySelectorAll(".menu-link");
 
 openMenuBtn.addEventListener("click", () => {
   menu.classList.remove("hidden");
@@ -11,8 +10,35 @@ closeMenuBtn.addEventListener("click", () => {
   menu.classList.add("hidden");
 });
 
-// menuLink.forEach((item) => {
-//   item.addEventListener("click", () => {
-//     menu.classList.add("hidden");
-//   });
-// });
+// Pricing page functionality
+const totalPlanPrice = document.getElementById("plan-total-price");
+const planPeriod = document.getElementById("plan-period");
+const planPrice = document.getElementById("plan-price");
+const discountText = document.querySelector("#discount");
+console.log(discountText);
+
+const yOffset = -100;
+const totalBox = document.querySelector(".payment-section__info-box");
+const y = totalBox.getBoundingClientRect().top + window.pageYOffset + yOffset;
+
+function CalculatePlanTotal(price, period, oldPrice) {
+  let discount = 0;
+  const total = (price * period).toFixed(2);
+  if (oldPrice !== 0) {
+    discount = (oldPrice * period - total).toFixed(2);
+    discountText.style.visiblity = "visible";
+    discountText.textContent = "Экономия $" + discount;
+  } else {
+    discountText.textContent = "";
+  }
+  planPeriod.textContent = period + "-месячный тариф";
+  planPrice.textContent = "$" + price + "/мес";
+
+  totalPlanPrice.textContent = "$" + total;
+  window.scrollTo({ top: y, behavior: "smooth" });
+  totalBox.classList.add("focus");
+  setTimeout(() => {
+    totalBox.classList.remove("focus");
+  }, 1000);
+  console.log("click");
+}
